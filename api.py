d47efa1b5f8d48d8d0bd47b98341d1dffa1cc10d
Wyssembh <wissembouhamda2007@gmail.com>
2026-05-12 15:16:51 +0100

    ajout de data

--- api.py
+++ api.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["POST"],
     allow_headers=["*"],
 )
